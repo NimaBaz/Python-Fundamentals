c844c2ca1ded8dac1d13a5d84cbf17ed4017d186
Nima Bazofti <nimabaz@yahoo.com>
2023-01-09 12:17:32 -0800

    hello_world commit

---
+++ Python Fundamentals/Fundamentals/introduction/hello_world.py
@@ -0,0 +1,16 @@
+statement = "Hello World"
+print(statement)
+
+name = "Nima Bazofti"
+print(name)
+print("Hello", name)
+
+num = 10
+print("Hello", num)
+print("Hello " + str(num))
+
+food_1 = "sushi"
+food_2 = "kabobs"
+print("I love to eat {} and {} ".format(food_1, food_2))
+print(f"I love to eat {food_1} and {food_2}")
+
