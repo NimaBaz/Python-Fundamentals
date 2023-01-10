b169156751092066d1ee8489700944b10eb13446
Nima Bazofti <nimabaz@yahoo.com>
2023-01-09 23:25:59 -0800

    function basics 2 commit

---
+++ Python Fundamentals/Fundamentals/fundamentals/function_basic2.py
@@ -0,0 +1,50 @@
+# def countdown(num):
+#     my_list = []
+#     for i in range(num, -1, -1):
+#         my_list.append(i)
+#     return my_list
+
+# print(countdown(10))
+
+# def print_and_return():
+#     my_list = [1, 2]
+#     print(my_list[0])
+#     return my_list[1]
+
+# print(print_and_return())
+
+# def first_plus_length():
+#     my_list = [1, 2, 3, 4, "love", "hate"]
+#     print(my_list[0])
+#     print(len(my_list))
+#     return (my_list[0] + len(my_list))
+
+# print(first_plus_length())
+
+# def values_greater_than_second():
+#     my_list = [1, 2, 3, 4, 5, 6, 7, 8]
+#     new_list = []
+
+#     for i in my_list:
+#         if (len(my_list) < 2):
+#             return False
+
+#         if (i > my_list[1]):
+#             newList.append(i)
+    
+#     print(len(new_list))
+#     return new_list
+
+# print(values_greater_than_second())
+
+# def this_length_and_that_value(size, value):
+#     my_list = []
+
+#     for i in range(size):
+#         my_list.append(value)
+#     return my_list
+
+# print(this_length_and_that_value(4,7))
+# print(this_length_and_that_value(6,2))
+
+
