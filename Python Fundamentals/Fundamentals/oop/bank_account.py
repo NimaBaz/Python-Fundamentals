9e98bd558210538fdcbb97389d35e379eb1db0fc
Nima Bazofti <nimabaz@yahoo.com>
2023-01-10 16:46:39 -0800

    bank account updated

--- Python Fundamentals/Fundamentals/oop/bank_account.py
+++ Python Fundamentals/Fundamentals/oop/bank_account.py
@@ -1,10 +1,14 @@
 class BankAccount:
     # don't forget to add some default values for these parameters!
-    def __init__(self, int_rate, balance): 
+    all_accounts = []
+
+    def __init__(self, user, int_rate, balance): 
         # your code here! (remember, instance attributes go here)
         # don't worry about user info here; we'll involve the User class soon
         self.rate = int_rate
         self.balance = balance
+        self.user = user
+        BankAccount.all_accounts.append(self)
 
     def deposit(self, amount):
         self.balance += amount
@@ -31,14 +35,21 @@ class BankAccount:
     def yield_interest(self):
         if self.balance > 0:
             self.balance += self.balance * self.rate
-            print(self.balance)
         else:
             print("Insufficient funds")
         return self
-        
-account1 = BankAccount(0.1, 2000)
-account1.deposit(500).withdraw(3000).display_account_info().yield_interest()
 
-account2 = BankAccount(10, 2000)
-account2.deposit(8000).withdraw(5000).display_account_info().yield_interest()
+    @classmethod
+    def all_balances(cls):
+        for account in cls.all_accounts:
+            print(f"{account.user} ${account.balance} and the interest rate is {account.rate}")
+
+
+account1 = BankAccount("Nima's Account has: ", 0.1, 2000)
+account1.deposit(500).withdraw(3000).yield_interest().display_account_info()
+
+account2 = BankAccount("Yooo's Account has: ", 10, 2000)
+account2.deposit(8000).withdraw(5000).yield_interest().display_account_info()
+
+BankAccount.all_balances()
 
