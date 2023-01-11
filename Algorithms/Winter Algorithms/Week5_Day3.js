4509fad2686dde0ccc6704761f9a32d244c26fe1
Nima Bazofti <nimabaz@yahoo.com>
2023-01-11 09:00:17 -0800

    algo commit

---
+++ Algorithms/Winter Algorithms/Week5_Day3.js
@@ -0,0 +1,39 @@
+/* 
+    String: Reverse
+    Given a string,
+    return a new string that is the given string reversed
+*/
+
+const str1 = "creature";
+const expected1 = "erutaerc";
+
+const str2 = "dog";
+const expected2 = "god";
+
+const str3 = "hello";
+const expected3 = "olleh";
+
+const str4 = "";
+const expected4 = "";
+
+/**
+ * Reverses the given str.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {string} str String to be reversed.
+ * @returns {string} The given str reversed.
+ */
+function reverseString(str) {
+    revList = [];
+
+    for (var i = str.length - 1; i >= 0; i--) {
+        // revList += str[i];
+        revList.push(str[i]);
+    }
+    return revList.join("")
+}
+
+console.log(reverseString(str1))
+console.log(reverseString(str2))
+console.log(reverseString(str3))
+console.log(reverseString(str4))
