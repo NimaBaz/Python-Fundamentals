3ec40a965044d28b1b1075a1d8bbe6cd643c66da
Nima Bazofti <nimabaz@yahoo.com>
2023-01-14 21:03:41 -0800

    counter update

--- Python Fundamentals/Flask/Counter/server.py
+++ Python Fundamentals/Flask/Counter/server.py
@@ -5,19 +5,24 @@ from flask import Flask, render_template, request, redirect, session
 app = Flask(__name__) 
 app.secret_key = 'TheSosaGuwop'
 
-@app.route('/users', methods=['POST'])
-def create_user():
-    print("Got Post Info")
-    # Here we add two properties to session to store the name and email
-    session['username'] = request.form['name']
-    session['useremail'] = request.form['email']
-    return redirect('/show')
+@app.route('/')
+def index():
+    if "view" in session:
+        session['view'] += 1
+    else:
+        session['view'] = 0
+    return render_template("index.html")
 
 
-@app.route('/show')
-def show_user():
-    return render_template('show.html', name_on_template=session['username'], email_on_template=session['useremail'])
+@app.route('/Add2')
+def vists():
+    session['view'] += 1
+    return redirect('/')
 
+@app.route('/destroy_session')
+def clear():
+    session.clear()
+    return redirect('/')
 
 
 
